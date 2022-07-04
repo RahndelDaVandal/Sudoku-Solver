fd7f344b73509fb3ac54966ed969c5ffa2d8219b
Connor Sahleen <Connor.Sahleen@gmail.com>
2022-07-04 07:38:22 -0500

    cleaning up files to start Poetry TDD version of sudoku solver

--- src/sudoku_solver/test.py
+++
@@ -1,9 +0,0 @@
-from rich.layout import Layout
-from rich.panel import Panel
-from rich import print
-from rich import box
-
-inner = Panel('HELLO WORLD', title='title', subtitle='subtitle')
-outer = Panel(inner, box=box.SIMPLE, padding=5, expand=True)
-
-print(outer)
